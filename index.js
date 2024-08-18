2403373615c0b851e471bea56d1eb608a26abfc0
hainweb <hainofficialweb@gmail.com>
2024-08-18 16:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const express = require('express')
 const cors = require('cors')
-const { Server } = require('socket.io')
 require('dotenv').config()
 const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
 
-const server = http.createServer(app);
-
+// const app = express()
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
-const io = new Server(server, {
-  cors: {
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-  transports: ['websocket', 'polling']
-});
-
-io.on('connection', (socket) => {
-  console.log('New client connected', socket.id);
-  socket.on('disconnect', (reason) => {
-    console.log('Client disconnected', socket.id, reason);
-  });
-});
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
 app.use(express.json())
 app.use(cookiesParser())
 
