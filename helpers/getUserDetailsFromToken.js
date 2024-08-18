0b20c7dd535e752e6c0b9c9c12bbbfcf281f27ee
hainweb <hainofficialweb@gmail.com>
2024-08-18 14:10:35 +0530

    Update getUserDetailsFromToken.js

--- helpers/getUserDetailsFromToken.js
+++ helpers/getUserDetailsFromToken.js
@@ -1,20 +1,23 @@
 const jwt = require('jsonwebtoken')
 const UserModel = require('../models/UserModel')
 
-const getUserDetailsFromToken = async(token)=>{
-    
-    if(!token){
-        return {
-            message : "session out",
-            logout : true,
-        }
+const getUserDetailsFromToken = async (token) => {
+    if (!token) {
+      console.log("No token provided");
+      return { message: "session out", logout: true };
     }
-
-    const decode = await jwt.verify(token,process.env.JWT_SECREAT_KEY)
-
-    const user = await UserModel.findById(decode.id).select('-password')
-
-    return user
-}
+    try {
+      const decode = await jwt.verify(token, process.env.JWT_SECREAT_KEY);
+      if (Date.now() >= decode.exp * 1000) {
+        console.log("Token expired");
+        return { message: "session expired", logout: true };
+      }
+      const user = await UserModel.findById(decode.id).select('-password');
+      return user;
+    } catch (error) {
+      console.error("Error verifying token:", error);
+      return { message: "invalid token", logout: true };
+    }
+  };
 
 module.exports = getUserDetailsFromToken
